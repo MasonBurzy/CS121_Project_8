941ec9313b109de045198c304b9cce45e4938ea0
MasonBurzy <mason.burzynski@bsu.edu>
2025-03-12 13:49:12 +0000

    upd guesser.java

--- Guesser.java
+++ Guesser.java
@@ -1,4 +1,22 @@
 // Guesser.java //
 
 
+public class Guesser {
+	public static void main(String[] args) {
+		Scanner scanner = new scanner(System.in);
 
+		String[] menuItems = {
+			"Exit",
+			"Human Guesser",
+			"Computer Guesser"
+
+		} // end menuItems
+
+		while (true) {
+			for (int i=0; i < menuItems.length; i++) {
+		
+		}
+	}
+
+
+}
